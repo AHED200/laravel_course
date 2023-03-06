2823429bebaa0c70c7be052dc7cb4c92142be76b
AHED200 <78029038+AHED200@users.noreply.github.com>
2023-03-06 23:46:26 +0300

    Add post controller and table(model) & Create form

---
+++ query_builder/resources/views/posts/create.blade.php
@@ -0,0 +1,10 @@
+<h1>Create new post</h1>
+
+<form>
+    <input type="text" name="title" placeholder="Enter title">
+    <br><br>
+    <input type="text" name="body" placeholder="Enter body">
+
+    <br><br>
+    <button type="submit">Submit</button>
+</form>
