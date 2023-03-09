6d2f978e3c8efe92bc1088c57443bd051085f354
AHED200 <78029038+AHED200@users.noreply.github.com>
2023-03-09 17:30:21 +0300

    Enable SoftDelete on Post model

--- eloquent/app/Models/Post.php
+++ eloquent/app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
@@ -13,4 +14,6 @@ class Post extends Model
     protected $fillable = ['title', 'body'];
     //OR this
     // protected $guarded = [];
+
+    use SoftDeletes;
 }
