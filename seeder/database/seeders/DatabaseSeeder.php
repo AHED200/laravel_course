02706443a7b159e8a13ea395c3a8564b173d96e1
AHED200 <78029038+AHED200@users.noreply.github.com>
2023-03-16 20:38:07 +0300

    Create factory for Post & User

--- seeder/database/seeders/DatabaseSeeder.php
+++ seeder/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,13 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+
+        Post::factory(30)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call([UserSeeder::class]);
+        // $this->call([UserSeeder::class]);
     }
 }
