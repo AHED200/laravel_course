6ae1d2a558187452615b23a128b922f151c7d93a
AHED200 <78029038+AHED200@users.noreply.github.com>
2023-03-19 02:06:59 +0300

    Use some laravel helpers function

---
+++ helpers/app/Http/Controllers/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        /**
+         * Examples of available helpers
+         */
+        // return User::all()->dd();
+        // return abort('500');
+
+
+    }
+}
